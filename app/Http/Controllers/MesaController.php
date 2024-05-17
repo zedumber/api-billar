aa989f05e0b6273b86ee5f212d347c6da9d7d58f
luis carlos bermudez diaz <127953422+zedumber@users.noreply.github.com>
2024-05-17 14:39:00 -0500

    billar

--- app/Http/Controllers/MesaController.php
+++ app/Http/Controllers/MesaController.php
@@ -8,14 +8,18 @@ class MesaController extends Controller
 {
     public function crearMesa(Request $request)
     {
-        $request->validate([
-            'nombre' => 'required|string',
-        ]);
-
-        $mesa = Mesa::create([
-            'nombre' => $request->nombre,
-        ]);
-
-        return response()->json($mesa, 201);
+        try {
+            $request->validate([
+                'nombre' => 'required|string',
+            ]);
+    
+            $mesa = Mesa::create([
+                'nombre' => $request->nombre,
+            ]);
+    
+            return response()->json($mesa, 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 400);
+        }
     }
 }
